fa778513cfd9fa1d05528fa7703e60ecacf8baed
Lilit Mkrtchyan <lilith_mkrtchyan@mail.ru>
2025-12-03 14:30:08 +0400

    Add fonts, images, CSS updates for project setup

--- src/moc/mockProducts.ts
+++ src/moc/mockProducts.ts
@@ -1,84 +1,274 @@
 import type { Product } from '../types/product';
+import bracelet1 from '../assets/img/bracelets/bracelet1.jpg';
+import bracelet2 from '../assets/img/bracelets/bracelet2.jpg';
+import bracelet3 from '../assets/img/bracelets/bracelet3.jpg';
+import bracelet4 from '../assets/img/bracelets/bracelet4.jpg';
+import bracelet5 from '../assets/img/bracelets/bracelet5.jpg';
+import bracelet6 from '../assets/img/bracelets/bracelet6.jpg';
+import bracelet7 from '../assets/img/bracelets/bracelet7.jpg';
+import bracelet8 from '../assets/img/bracelets/bracelet8.jpg';
+import cufflinks1 from '../assets/img/cufflinks/cufflinks1.jpg';
+import cufflinks2 from '../assets/img/cufflinks/cufflinks2.jpg';
+import cufflinks3 from '../assets/img/cufflinks/cufflinks3.jpg';
+import cufflinks4 from '../assets/img/cufflinks/cufflinks4.jpg';
+import cufflinks5 from '../assets/img/cufflinks/cufflinks5.jpg';
+import cufflinks6 from '../assets/img/cufflinks/cufflinks6.jpg';
+import cufflinks7 from '../assets/img/cufflinks/cufflinks7.jpg';
+import cufflinks8 from '../assets/img/cufflinks/cufflinks8.jpg';
+import necklace1 from '../assets/img/necklaces/necklace1.jpg';
+import necklace2 from '../assets/img/necklaces/necklace2.jpg';
+import necklace3 from '../assets/img/necklaces/necklace3.jpg';
+import necklace4 from '../assets/img/necklaces/necklace4.jpg';
+import necklace5 from '../assets/img/necklaces/necklace5.jpg';
+import necklace6 from '../assets/img/necklaces/necklace6.jpg';
+import ring1 from '../assets/img/rings/ring1.jpg';
+import ring2 from '../assets/img/rings/ring2.jpg';
+import ring3 from '../assets/img/rings/ring3.jpg';
+import ring4 from '../assets/img/rings/ring4.jpg';
+import ring5 from '../assets/img/rings/ring5.jpg';
+import ring6 from '../assets/img/rings/ring6.jpg';
+import ring7 from '../assets/img/rings/ring7.jpg';
+import ring8 from '../assets/img/rings/ring8.jpg';
 
 export const mockProducts: Product[] = [
   {
     id: '1',
-    name: 'Gold Ring',
-    price: 150,
-    image: '',
-    description: 'Elegant gold ring with diamonds',
-    category: 'jewelry',
+    name: 'Gold Bracelet',
+    price: 180,
+    image: bracelet1,
+    description: 'Luxury gold bracelet with elegant design',
+    category: 'bracelets',
   },
   {
     id: '2',
-    name: 'Silver Necklace',
+    name: 'Silver Bracelet',
     price: 120,
-    image: '',
-    description: 'Stylish silver necklace',
-    category: 'jewelry',
+    image: bracelet2,
+    description: 'Classic silver bracelet',
+    category: 'bracelets',
   },
   {
     id: '3',
-    name: 'Leather Wallet',
-    price: 50,
-    image: '',
-    description: 'Premium leather wallet',
-    category: 'accessories',
+    name: 'Diamond Bracelet',
+    price: 250,
+    image: bracelet3,
+    description: 'Bracelet with diamonds',
+    category: 'bracelets',
   },
   {
     id: '4',
-    name: 'Diamond Earrings',
-    price: 200,
-    image: '',
-    description: 'Sparkling diamond earrings',
-    category: 'jewelry',
+    name: 'Platinum Bracelet',
+    price: 300,
+    image: bracelet4,
+    description: 'Premium platinum bracelet',
+    category: 'bracelets',
   },
   {
     id: '5',
-    name: 'Sunglasses',
-    price: 80,
-    image: '',
-    description: 'Fashion sunglasses with UV protection',
-    category: 'accessories',
+    name: 'Leather Bracelet',
+    price: 60,
+    image: bracelet5,
+    description: 'Stylish leather bracelet',
+    category: 'bracelets',
   },
   {
     id: '6',
-    name: 'Platinum Bracelet',
-    price: 180,
-    image: '',
-    description: 'Luxury platinum bracelet',
-    category: 'jewelry',
+    name: 'Chain Bracelet',
+    price: 90,
+    image: bracelet6,
+    description: 'Chain-style bracelet',
+    category: 'bracelets',
   },
   {
     id: '7',
-    name: 'Backpack',
-    price: 60,
-    image: '',
-    description: 'Stylish travel backpack',
-    category: 'accessories',
+    name: 'Beaded Bracelet',
+    price: 50,
+    image: bracelet7,
+    description: 'Handmade beaded bracelet',
+    category: 'bracelets',
   },
   {
     id: '8',
-    name: 'Pearl Necklace',
-    price: 130,
-    image: '',
-    description: 'Classic pearl necklace',
-    category: 'jewelry',
+    name: 'Gemstone Bracelet',
+    price: 140,
+    image: bracelet8,
+    description: 'Bracelet with natural gemstones',
+    category: 'bracelets',
   },
   {
     id: '9',
-    name: 'Watch',
-    price: 250,
-    image: '',
-    description: 'Luxury wristwatch',
-    category: 'accessories',
+    name: 'Gold Cufflinks',
+    price: 150,
+    image: cufflinks1,
+    description: 'Elegant gold cufflinks',
+    category: 'cufflinks',
   },
   {
     id: '10',
-    name: 'Gold Earrings',
+    name: 'Silver Cufflinks',
+    price: 110,
+    image: cufflinks2,
+    description: 'Polished silver cufflinks',
+    category: 'cufflinks',
+  },
+  {
+    id: '11',
+    name: 'Black Onyx Cufflinks',
     price: 170,
-    image: '',
-    description: 'Elegant gold earrings',
-    category: 'jewelry',
+    image: cufflinks3,
+    description: 'Cufflinks with black onyx',
+    category: 'cufflinks',
+  },
+  {
+    id: '12',
+    name: 'Mother of Pearl Cufflinks',
+    price: 180,
+    image: cufflinks4,
+    description: 'Premium mother of pearl cufflinks',
+    category: 'cufflinks',
+  },
+  {
+    id: '13',
+    name: 'Modern Steel Cufflinks',
+    price: 90,
+    image: cufflinks5,
+    description: 'Minimalist steel cufflinks',
+    category: 'cufflinks',
+  },
+  {
+    id: '14',
+    name: 'Engraved Cufflinks',
+    price: 130,
+    image: cufflinks6,
+    description: 'Custom engraved cufflinks',
+    category: 'cufflinks',
+  },
+  {
+    id: '15',
+    name: 'Rubber Cufflinks',
+    price: 70,
+    image: cufflinks7,
+    description: 'Sport-style cufflinks',
+    category: 'cufflinks',
+  },
+  {
+    id: '16',
+    name: 'Diamond Cufflinks',
+    price: 260,
+    image: cufflinks8,
+    description: 'Cufflinks with diamonds',
+    category: 'cufflinks',
+  },
+  {
+    id: '17',
+    name: 'Gold Necklace',
+    price: 200,
+    image: necklace1,
+    description: 'Elegant gold necklace',
+    category: 'necklaces',
+  },
+  {
+    id: '18',
+    name: 'Silver Necklace',
+    price: 150,
+    image: necklace2,
+    description: 'Stylish silver necklace',
+    category: 'necklaces',
+  },
+  {
+    id: '19',
+    name: 'Pearl Necklace',
+    price: 220,
+    image: necklace3,
+    description: 'Classic pearl necklace',
+    category: 'necklaces',
+  },
+  {
+    id: '20',
+    name: 'Diamond Pendant Necklace',
+    price: 300,
+    image: necklace4,
+    description: 'Necklace with diamond pendant',
+    category: 'necklaces',
+  },
+  {
+    id: '21',
+    name: 'Chain Necklace',
+    price: 80,
+    image: necklace5,
+    description: 'Simple chain necklace',
+    category: 'necklaces',
+  },
+  {
+    id: '22',
+    name: 'Gemstone Necklace',
+    price: 180,
+    image: necklace6,
+    description: 'Necklace with gemstones',
+    category: 'necklaces',
+  },
+  {
+    id: '23',
+    name: 'Gold Ring',
+    price: 150,
+    image: ring1,
+    description: 'Elegant gold ring',
+    category: 'rings',
+  },
+  {
+    id: '24',
+    name: 'Silver Ring',
+    price: 120,
+    image: ring2,
+    description: 'Stylish silver ring',
+    category: 'rings',
+  },
+  {
+    id: '25',
+    name: 'Diamond Ring',
+    price: 270,
+    image: ring3,
+    description: 'Ring with diamonds',
+    category: 'rings',
+  },
+  {
+    id: '26',
+    name: 'Engagement Ring',
+    price: 350,
+    image: ring4,
+    description: 'Luxury engagement ring',
+    category: 'rings',
+  },
+  {
+    id: '27',
+    name: 'Platinum Ring',
+    price: 300,
+    image: ring5,
+    description: 'Premium platinum ring',
+    category: 'rings',
+  },
+  {
+    id: '28',
+    name: 'Gemstone Ring',
+    price: 190,
+    image: ring6,
+    description: 'Ring with gemstones',
+    category: 'rings',
+  },
+  {
+    id: '29',
+    name: 'Minimalist Ring',
+    price: 80,
+    image: ring7,
+    description: 'Simple minimalist ring',
+    category: 'rings',
+  },
+  {
+    id: '30',
+    name: 'Vintage Ring',
+    price: 160,
+    image: ring8,
+    description: 'Vintage-style ring',
+    category: 'rings',
   },
 ];
